30c2359eda6dc3ddf173e4be0c092f2abe435cc9
jungyu333 <jungyu3826@gmail.com>
2025-06-01 21:15:16 +0900

    feat : Add Design system colors & fonts

---
+++ styles/Fonts.ts
@@ -0,0 +1,28 @@
+export const fonts = {
+  headline: {
+    h1: {
+      fontFamily: 'Inter-Bold',
+      fontSize: 30,
+    },
+    h2: {
+      fontFamily: 'Inter-Bold',
+      fontSize: 20,
+    },
+    h3: {
+      fontFamily: 'Inter-Bold',
+      fontSize: 20,
+    },
+    h4: {
+      fontFamily: 'Inter-Medium',
+      fontSize: 20,
+    },
+  },
+  Btn1: {
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 16,
+  },
+  Btn2: {
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 14,
+  },
+};
