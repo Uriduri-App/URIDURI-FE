c01c1c1862c20d684747ffa4247ab4a11c87a256
jungyu333 <jungyu3826@gmail.com>
2025-06-01 21:24:10 +0900

    feat : Auth Page

---
+++ app/auth/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+
+export default function AuthScreen() {
+  return (
+    <SafeAreaView>
+      <Text>인증 홈</Text>
+    </SafeAreaView>
+  );
+}
